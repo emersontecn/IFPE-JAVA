eb19013a7f6f93fdb6500c8c77e2f246d8f5d7b9
Emerson Silva <emerson0712002@gmail.com>
2023-08-08 21:49:12 -0300

    confirmado

---
+++ SegundaAula/src/loop.java
@@ -0,0 +1,14 @@
+public class loop {
+
+	public static void main(String[] args) {
+	int x = 1;	
+	System.out.println("before the loop");
+	while (x < 4){
+		System.out.println("In the loop");
+		System.out.println("Value of x is " + x);
+		 x = x + 1;
+		
+		}
+	System.out.println("This is after the loop");
+	}
+}
